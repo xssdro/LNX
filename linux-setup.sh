df764bbeab745d3df4ecb58a74fa36a6ca7b1c2b
xssdro <85429129+xssdro@users.noreply.github.com>
2021-06-19 13:06:57 +0700

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -2,18 +2,36 @@
 #!/bin/bash
 
 
+ #Create New Account
+ useradd -m exdeusarch
+ adduser exdeusarch sudo
+ echo -e "kurama9\nkurama9" | passwd exdeusarch
+ echo "New Account Created"
 
-useradd -m exdeusarch
-adduser exdeusarch sudo
+ #Change Root Password
+ echo "kurama9" | passwd --stdin > /dev/null
 
-passwd exdeusarch
+ #Setup sshd_config
+ rm -f /etc/ssh/sshd_config*
+ cat <<'SSHCONFIG' > /etc/ssh_sshd_config
+#SSHCONFIG
+HostKey /etc/ssh/ssh_host_rsa_key
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostKey /etc/ssh/ssh_host_ed25519_key
+LogLevel VERBOSE
+Subsystem sftp /usr/lib/ssh/sftp-server -f AUTHPRIV -l INFO
+PermitRootLogin yes
+PasswordAuthentication yes
+PubkeyAuthentication yes
+PermitEmptyPassword no
+UsePrivilegeSeparation sandbox
+UsePAM yes
+SSHCONFIG
 
-passwd
+ #Restart SSH
+ service ssh restart
 
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-chmod +x ./ngrok
-
-./ngrok authtoken 
-
-./ngrok tcp 22
+ #Download Ngrok
+ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+ unzip ngrok-stable-linux-386.zip
+ chmod +x ./ngrok
